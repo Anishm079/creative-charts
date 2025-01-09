c55f408157b459223a5b701c3bfef470ff8c35db
Anish Ahammad Mulla <anishmulla7@gmail.com>
2025-01-10 03:58:38 +0530

    new tasks created

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 // import ChartComponent from './components/AGCharts/01-03-2025/MultipleRangeBars'
 // import Chart from "./components/D3Charts/Reworks/01-09-2025/IcelandicPopulationByAge";
-import ChartComponent from "./components/AGCharts/past/HorizontalWaterfall.tsx"
+// import ChartComponent from "./components/AGCharts/past/HorizontalWaterfall.tsx"
 // import ChordChart from "./components/D3Charts/Reworks/01-04-2025/RadialClusterTree";
 // import Chart from "./components/D3Charts/Reworks/01-04-2025/CostPerGallon";
 import "ag-charts-enterprise"
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <div style={{background:"linear-gradient(120deg, #5fbaa6, #d657ae, #ff8190)",width:"100dvw",height:"100dvh"}}>
-      <ChartComponent />
+      {/* <ChartComponent /> */}
     </div>
   )
 }
