883e74da67af8156f625b47641b1a957768ef448
Anish Ahammad Mulla <anishmulla7@gmail.com>
2025-01-16 19:35:42 +0530

    reworks

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import './App.css'
 // import MyResponsiveStream from './components/01-13-2025/PerformanceMeasuresInAnEventForDifferentPersons'
-import GaugeChart from './components/01-15-2025/VehicleSpeedMeter'
+import GaugeChart from './components/01-15-2025/TemperatureMeter'
 import "ag-charts-enterprise"
 
 function App() {
 
   return (
     <div style={{background:"linear-gradient(to right, #592478,#284440)",width:"100dvw",height:"100dvh"}}>
-      <h2 style={{color:"white"}}>speed meter</h2>
+      <h2 style={{color:"white"}}>temperature meter</h2>
       <div style={{height:"90%",width:"100dvw",display:"flex",alignItems:"center",justifyContent:"center"}} >
         <GaugeChart />
       </div>
--- src/utils/temp.ts
+++ src/utils/temp.ts
@@ -1,7 +1,5 @@
 [
-  { actual: 30,expected: 60, name: 'Visit' },
-  { actual: 10,expected: 40, name: 'Inquiry' },
-  { actual: 5, expected: 20, name: 'Order' },
-  { actual: 50,expected: 80, name: 'Click' },
-  { actual: 80,expected: 100, name: 'Show' }
+  {
+    temprature:"9.42 celsius",
+  }
 ]
