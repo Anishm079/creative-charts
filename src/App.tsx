a32fff878ce2c3fd816de594fba49ab9c6009d01
Anish Ahammad Mulla <anishmulla7@gmail.com>
2025-01-16 19:57:11 +0530

    rework done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 // import ChartComponent from './components/AGCharts/01-03-2025/MultipleRangeBars'
 // import Chart from "./components/D3Charts/Reworks/01-09-2025/IcelandicPopulationByAge";
-import ChartComponent from "./components/AGCharts/past/SimpleRangeArea.tsx"
+import ChartComponent from "./components/AGCharts/past/PlayersMarketValue.tsx"
 // import ChartComponent from "./components/AGCharts/12-20-2024/GroupedCategoryAxisCombination.tsx"
 // import ChordChart from "./components/D3Charts/Reworks/01-04-2025/RadialClusterTree";
 // import Chart from "./components/D3Charts/Reworks/01-04-2025/CostPerGallon";
@@ -10,7 +10,7 @@ import "ag-charts-enterprise"
 function App() {
 
   return (
-    <div style={{background:"linear-gradient(120deg, #ff00f7, #51ff00, #81ffff)",width:"100dvw",height:"100dvh"}}>
+    <div style={{background:"linear-gradient(120deg, #ff00f7, #a8ff7f, #81ffff)",width:"100dvw",height:"100dvh"}}>
       <ChartComponent />
     </div>
   )
